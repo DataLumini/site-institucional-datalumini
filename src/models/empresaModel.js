f58f8480795217c545145625b9c0dc704567bf38
gabrielGalhiardoFarina <gabriel.farina@sptech.school>
2026-05-20 20:21:55 -0300

    feat: adicionando rota para consumo de dados da dash principal

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,31 +1,31 @@
 var database = require("../database/config");
 
 function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+  var instrucaoSql = `SELECT * FROM Empresa WHERE id = '${id}'`;
 
   return database.executar(instrucaoSql);
 }
 
 function listar() {
-  var instrucaoSql = `SELECT id, razaosocial, cnpj, codigo_ativacao FROM empresa`;
+  var instrucaoSql = `SELECT id, razaosocial, cnpj, codigo_ativacao FROM Empresa`;
 
   return database.executar(instrucaoSql);
 }
 
 function buscarPorCnpj(cnpj) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+  var instrucaoSql = `SELECT * FROM Empresa WHERE cnpj = '${cnpj}'`;
 
   return database.executar(instrucaoSql);
 }
 
 function cadastrar(razaoSocial, cnpj) {
-  var instrucaoSql = `INSERT INTO empresa (razaosocial, cnpj) VALUES ('${razaoSocial}', '${cnpj}')`;
+  var instrucaoSql = `INSERT INTO Empresa (razaosocial, cnpj) VALUES ('${razaoSocial}', '${cnpj}')`;
 
   return database.executar(instrucaoSql);
 }
 
 function buscarPorNome(nome) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE razaosocial = '${nome}'`;
+  var instrucaoSql = `SELECT * FROM Empresa WHERE razaosocial = '${nome}'`;
   return database.executar(instrucaoSql);
 }
 
