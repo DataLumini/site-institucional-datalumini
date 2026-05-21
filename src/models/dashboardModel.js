0e5376bf5afdabd02f187122705c873c111dbfcc
gabrielGalhiardoFarina <gabriel.farina@sptech.school>
2026-05-21 12:49:51 -0300

    refactor: remodelando o model da dash para usar a view

--- src/models/dashboardModel.js
+++ src/models/dashboardModel.js
@@ -1,33 +1,11 @@
 var database = require("../database/config")
 
 function listar(idUsuario) {
-    console.log("ACESSEI O DASHBOARD MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPrateleira():", idUsuario );
-            var instrucaoSql = `
-        SELECT 
-            es.nome AS Estufa,
-            s.nome AS Setor,
-            est.numeroIdentificador AS Estante,
-            p.numeroIdentificador AS Prateleira,
-            sen.nome AS Sensor,
-            l.frequenciaLuminosidade AS FrequenciaLuminosa,
-            l.dtCaptacaoDados AS DataLeitura
-        FROM Usuario u
-        JOIN Empresa e 
-            ON u.fkEmpresa = e.idEmpresa
-        JOIN Estufa es 
-            ON e.idEmpresa = es.fkEmpresa
-        JOIN Setor s 
-            ON es.idEstufa = s.fkEstufa
-        JOIN Estante est 
-            ON s.idSetor = est.fkSetor
-        JOIN Prateleira p 
-            ON est.idEstante = p.fkEstante
-        JOIN Sensor sen 
-            ON p.idPrateleira = sen.fkPrateleira
-        JOIN Leitura l 
-            ON sen.idSensor = l.fkSensor
-        WHERE 
-            u.idUsuario = ${idUsuario};
+    console.log("ACESSEI O DASHBOARD MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPrateleira():", idUsuario);
+    var instrucaoSql = `
+          SELECT * 
+        FROM vw_obter_dados_dash_principal 
+        WHERE idUsuario = ${idUsuario};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
