d360452f37262b17c213db37717c5e6fa1ea94ad
GuiPastorello <guilherme.pastorello@sptech.school>
2026-05-28 20:39:04 -0300

    Adicionando funções do gerenciamento de usuario ao controller

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -5,7 +5,7 @@ var empresaModel = require("../models/empresaModel");
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    
+
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
@@ -123,69 +123,48 @@ function cadastrar(req, res) {
     }
 }
 
-// function listarUsuariosPorEmpresa(req, res) {
-//     var id_empresa = req.params.idEmpresa;
-    
-//     if (email == undefined) {
-//         res.status(400).send("Seu email está undefined!");
-//     } else if (senha == undefined) {
-//         res.status(400).send("Sua senha está indefinida!");
-//     } else {
+function listarUsuariosPorEmpresa(req, res) {
+    var idEmpresa = req.params.idEmpresa;
 
-//         usuarioModel.autenticar(email, senha)
-//             .then(
-//                 function (resultadoAutenticar) {
-//                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-//                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+    usuarioModel.buscarPorId(idEmpresa).then((resultado) => {
+        res.status(200).json(resultado);
+    });
+}
 
-//                     if (resultadoAutenticar.length == 1) {
-//                         console.log(resultadoAutenticar);
+function listarEstufasDoUsuario(req, res) {
+    var idUsuario = req.params.idUsuario;
 
-//                         /* aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-//                             .then((resultadoAquarios) => {
-//                                 if (resultadoAquarios.length > 0) {
-//                                     res.json({
-//                                         id: resultadoAutenticar[0].id,
-//                                         email: resultadoAutenticar[0].email,
-//                                         nome: resultadoAutenticar[0].nome,
-//                                         senha: resultadoAutenticar[0].senha,
-//                                         aquarios: resultadoAquarios
-//                                     });
-//                                 } else {
-//                                     res.status(204).json({ aquarios: [] });
-//                                 }
-//                             }) */
+    usuarioModel.listarEstufasDoUsuario(idUsuario).then((resultado) => {
+        res.status(200).json(resultado);
+    });
+}
 
-//                         // RESPOSTA MOCKADA SÓ PARA LIBERAR O ACESSO:
-//                         res.json({
-//                             id: resultadoAutenticar[0].id,
-//                             email: resultadoAutenticar[0].email,
-//                             nome: resultadoAutenticar[0].nome,
-//                             empresaId: resultadoAutenticar[0].empresaId,
-//                             aquarios: []
-//                         });
+function adicionarEstufa(req, res) {
 
-//                     } else if (resultadoAutenticar.length == 0) {
-//                         res.status(403).send("Email e/ou senha inválido(s)");
-//                     } else {
-//                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-//                     }
-//                 }
-//             ).catch(
-//                 function (erro) {
-//                     console.log(erro);
-//                     console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-//                     res.status(500).json(erro.sqlMessage);
-//                 }
-//             );
-//     }
+    var idUsuario = req.body.idUsuarioServer;
+    var idEstufa = req.body.idEstufaServer;
 
-// }
+    empresaModel.adicionarEstufa(idUsuario, idEstufa).then((resultado) => {
+        res.status(201).json(resultado);
+    });
 
+}
 
+function retirarEstufa(req, res) {
 
+    var idUsuario = req.body.idUsuarioServer;
+    var idEstufa = req.body.idEstufaServer;
+
+    empresaModel.retirarEstufa(idUsuario, idEstufa).then((resultado) => {
+        res.status(201).json(resultado);
+    });
+
+}
 
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    listarUsuariosPorEmpresa,
+    listarEstufasDoUsuario,
+    adicionarEstufa
 }
