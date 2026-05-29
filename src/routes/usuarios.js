a2b49cd9ddc98e77ea72b345e54a00cc35c64425
gabrielGalhiardoFarina <gabriel.farina@sptech.school>
2026-05-29 14:32:28 -0300

    fix: padronizando as rodas do usuario

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -12,19 +12,19 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
-router.get("/listar-usuarios-empresa/:empresaId", function(req, res){
+router.get("/listar/usuarios/empresa/:empresaId", function(req, res){
     usuarioController.listarUsuariosPorEmpresa(req, res);
 });
 
-router.get("/estufas-por-usuarios/:usuarioId", function(req, res){
+router.get("/listar/estufas/usuario/:usuarioId", function(req, res){
     usuarioController.listarEstufasDoUsuario(req, res);
 });
 
-router.post("/adicionarEstufa", function(req, res){
+router.post("/adicionar/estufa", function(req, res){
     usuarioController.adicionarEstufa(req, res)
 });
 
-router.delete("/retirarEstufa", function(req, res){
+router.delete("/retirar/estufa", function(req, res){
     usuarioController.retirarEstufa(req, res)
 })
 module.exports = router;
