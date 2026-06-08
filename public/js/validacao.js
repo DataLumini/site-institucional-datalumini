71f7ae2647a87c54eb517023701e607bfe5bf26e
Kaue-Tamura27 <kaue.gomes@sptech.school>
2026-06-08 13:32:29 -0300

    Alterando o caminho para validar sessão

--- public/js/validacao.js
+++ public/js/validacao.js
@@ -5,7 +5,7 @@ function validarSessao() {
 
    if (nome == null || email == null || id == null) {
        alert("Para visualizar esta página, você precisa estar conectado à sua conta. Por favor, faça o login para continuar.")
-       window.location = "TelaLogin.html"
+       window.location = "telaLogin.html"
    } else {
        console.log('tudo ok!')
    }
