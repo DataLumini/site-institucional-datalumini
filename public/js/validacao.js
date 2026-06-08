92476036fc7e8d081271a2d5d0f500a5f31bed88
Kaue-Tamura27 <kaue.gomes@sptech.school>
2026-06-08 13:16:59 -0300

    Alterando cores das barras para melhor identificação de problemas

--- public/js/validacao.js
+++ public/js/validacao.js
@@ -1,14 +1,14 @@
 function validarSessao() {
-    const nome = sessionStorage.NOME_USUARIO;
-    const email = sessionStorage.EMAIL_USUARIO;
-    const id = sessionStorage.ID_USUARIO;
+   const nome = sessionStorage.NOME_USUARIO;
+   const email = sessionStorage.EMAIL_USUARIO;
+   const id = sessionStorage.ID_USUARIO;
 
-    if (nome == null || email == null || id == null) {
-        alert("Para visualizar esta página, você precisa estar conectado à sua conta. Por favor, faça o login para continuar.")
-        window.location = "TelaLogin.html"
-    } else {
-        console.log('tudo ok!')
-    }
+   if (nome == null || email == null || id == null) {
+       alert("Para visualizar esta página, você precisa estar conectado à sua conta. Por favor, faça o login para continuar.")
+       window.location = "TelaLogin.html"
+   } else {
+       console.log('tudo ok!')
+   }
 }
 
 function verificarRegraUsuario() {
