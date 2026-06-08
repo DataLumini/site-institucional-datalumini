f5e0184ce764ee3485f7c0eb9de6e3f1d7d12909
Kaue-Tamura27 <kaue.gomes@sptech.school>
2026-06-08 09:55:25 -0300

    adicionando legenda na dash específica

--- public/js/validacao.js
+++ public/js/validacao.js
@@ -1,15 +1,15 @@
-//function validarSessao() {
-//    const nome = sessionStorage.NOME_USUARIO;
-//    const email = sessionStorage.EMAIL_USUARIO;
-//    const id = sessionStorage.ID_USUARIO;
-//
-//    if (nome == null || email == null || id == null) {
-//        alert("Para visualizar esta página, você precisa estar conectado à sua conta. Por favor, faça o login para continuar.")
-//        window.location = "TelaLogin.html"
-//    } else {
-//        console.log('tudo ok!')
-//    }
-//}
+function validarSessao() {
+    const nome = sessionStorage.NOME_USUARIO;
+    const email = sessionStorage.EMAIL_USUARIO;
+    const id = sessionStorage.ID_USUARIO;
+
+    if (nome == null || email == null || id == null) {
+        alert("Para visualizar esta página, você precisa estar conectado à sua conta. Por favor, faça o login para continuar.")
+        window.location = "TelaLogin.html"
+    } else {
+        console.log('tudo ok!')
+    }
+}
 
 function verificarRegraUsuario() {
     let regraUsuario = sessionStorage.REGRA_USUARIO;
